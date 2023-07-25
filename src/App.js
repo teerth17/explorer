3cc2792663a770e4baa8a29f6c4563de7ad1d102
Teerth Patel <pteerth989@gmail.com>
2023-07-25 23:54:42 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
+import { Alchemy, Network } from "alchemy-sdk";
+import { useEffect, useState } from "react";
 
-import './App.css';
+import "./App.css";
 
 // Refer to the README doc for more information about using API
 // keys in client-side code. You should never do this in production
 // level code.
 const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
-  network: Network.ETH_MAINNET,
+  network: Network.ETH_SEPOLIA,
 };
-
 
 // In this week's lessons we used ethers.js. Here we are using the
 // Alchemy SDK is an umbrella library with several different packages.
@@ -26,11 +25,35 @@ function App() {
     async function getBlockNumber() {
       setBlockNumber(await alchemy.core.getBlockNumber());
     }
-
     getBlockNumber();
   });
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  async function getBlockTransactionHash(hash) {
+    let txHash = await alchemy.core.getBlockWithTransactions(hash);
+    console.log(txHash);
+    return txHash;
+  }
+
+  return (
+    <div className="App">
+      Block Number: {blockNumber}
+      <>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            getBlockTransactionHash(event.target.elements.txhash.value);
+            event.target.elements.txhash.value = "";
+          }}
+        >
+          <label>
+            TransactionHash:
+            <input type="number" name="txhash" />
+            <button type="submit"> Get Block TxHash</button>
+          </label>
+        </form>
+      </>
+    </div>
+  );
 }
 
 export default App;
